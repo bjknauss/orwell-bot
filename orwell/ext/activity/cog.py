4b37f63e7e53af87071e5357b1c3fcc714202e8b
Brenden Knauss <brenden.knauss@gmail.com>
2019-05-04 13:02:29 -0700

    quick add last month

--- orwell/ext/activity/cog.py
+++ orwell/ext/activity/cog.py
@@ -5,26 +5,28 @@ from redis import Redis
 from dynaconf import settings
 from orwell.ext.activity import keys, utils, shared
 
+
 class ActivityCog:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.redis: Redis = Redis(
             host=settings.REDIS.host,
             port=settings.REDIS.port,
-            db=settings.REDIS.db
-        )
+            db=settings.REDIS.db)
 
-
-    def get_result_set(self, key: str, start=0, num=1000, role_filter=None) -> shared.RawResultSet:
-        results = self.redis.zrevrangebyscore(key, '+inf', 0, withscores=True, start=start, num=num)
+    def get_result_set(self, key: str, start=0, num=1000,
+                       role_filter=None) -> shared.RawResultSet:
+        results = self.redis.zrevrangebyscore(
+            key, '+inf', 0, withscores=True, start=start, num=num)
         if isinstance(role_filter, discord.Role):
             results = utils.filter_by_role(results, role_filter)
         return results
 
-
     @commands.command(aliases=['top_month', 'mtop'], name='top')
     @commands.guild_only()
-    async def top_month(self, ctx: commands.Context, role: Optional[discord.Role] = None):
+    async def top_month(self,
+                        ctx: commands.Context,
+                        role: Optional[discord.Role] = None):
         key = keys.monthly(ctx.message)
         results = self.get_result_set(key, role_filter=role)
         results = results[:25]
@@ -34,6 +36,20 @@ class ActivityCog:
         resp = self.print_top_results(ctx, results, title=title)
         await ctx.send(resp)
 
+    @commands.command(aliases=['last_month', 'mlast'], name='last')
+    @commands.guild_only()
+    async def last_month(self,
+                         ctx: commands.Context,
+                         role: Optional[discord.Role] = None):
+        last_month = utils.get_previous_month(ctx)
+        key = keys.monthly(ctx.message, last_month)
+        results = self.get_result_set(key, role_filter=role)
+        results = results[:25]
+        results = utils.parse_results(self.bot, results)
+
+        title = last_month.strftime('Top - %b')
+        resp = self.print_top_results(ctx, results, title=title)
+        await ctx.send(resp)
 
     async def on_message(self, message: discord.Message):
         if not isinstance(message.channel, discord.TextChannel):
@@ -55,8 +71,11 @@ class ActivityCog:
         self.redis.zadd(monthly, user_dict, incr=True)
         self.redis.zadd(total, user_dict, incr=True)
 
-
-    def print_top_results(self, ctx: commands.Context, results: shared.ResultSet, start=1, title='Top Active'):
+    def print_top_results(self,
+                          ctx: commands.Context,
+                          results: shared.ResultSet,
+                          start=1,
+                          title='Top Active'):
         response = f'__**{title}**__:\n\n'
         is_staff = utils.is_staff(ctx.author)
 
@@ -64,7 +83,6 @@ class ActivityCog:
             response += utils.print_top_result_item(count, value, is_staff)
 
         return response
-
 
     def __unload(self):
         del self.redis
--- orwell/ext/activity/utils.py
+++ orwell/ext/activity/utils.py
@@ -1,5 +1,7 @@
 from typing import Tuple, Sequence, Union
 import discord
+from discord.ext.commands import Context
+from datetime import date, datetime, timedelta
 from redis import Redis
 from .shared import RawResultSet, RawResultItem, ResultSet, ResultItem
 
@@ -9,7 +11,8 @@ def is_staff(user: discord.Member) -> bool:
     return user.guild_permissions.ban_members
 
 
-def parse_results(client: discord.Client, result_set: RawResultSet) -> ResultSet:
+def parse_results(client: discord.Client,
+                  result_set: RawResultSet) -> ResultSet:
     '''Transforms a redis sorted set of (user id, score) into (User, score)
 
         returns Sequence[(discord.User, int)]
@@ -24,12 +27,15 @@ def parse_results(client: discord.Client, result_set: RawResultSet) -> ResultSet
         res.append((user, score))
     return res
 
+
 def filter_by_role(results: RawResultSet, role: discord.Role) -> RawResultSet:
     '''Filter Redis Result Set by Role.'''
     role_member_ids = [mem.id for mem in role.members]
-    return [ item for item in results if int(item[0]) in role_member_ids]
+    return [item for item in results if int(item[0]) in role_member_ids]
 
-def print_top_result_item(count: int, result_item: ResultItem, staff: bool) -> str:
+
+def print_top_result_item(count: int, result_item: ResultItem,
+                          staff: bool) -> str:
     '''Helper function for printing one result item for top command.'''
     user = result_item[0]
     score = result_item[1]
@@ -39,3 +45,26 @@ def print_top_result_item(count: int, result_item: ResultItem, staff: bool) -> s
     staff_line = f'\t| `<@{user_id}>`' if staff else ''
     response = f'{count:2}. **{name:20.16}** | {score:6d}{staff_line}\n'
     return response
+
+
+def get_previous_month(date_resolvable=date.today()):
+    d = resolve_date(date_resolvable)
+    return d.replace(day=1) - timedelta(days=1)
+
+
+def resolve_date(date_resolvable):
+
+    if issubclass(date_resolvable, date):
+        return date_resolvable
+
+    return get_date_from_discord_obj(date_resolvable)
+
+
+def get_date_from_discord_obj(date_resolvable):
+    if isinstance(date_resolvable, Context):
+        return date_resolvable.message.created_at
+
+    if isinstance(date_resolvable, discord.Message):
+        return date_resolvable.created_at
+
+    return None
